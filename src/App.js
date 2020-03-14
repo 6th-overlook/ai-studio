f43281efc7d6b352f33ccbb805996528c7d95605
6th-overlook <6th.overlook@gmail.com>
2020-03-14 22:46:22 +0000

    AI dev studio - starter template + header and description

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1>AI Studio</h1>
+        
+        <p>Initial state for an application I will use to my own home-grown AI framework</p>
+      </header>
+    </div>
+        )
+  }
+}
